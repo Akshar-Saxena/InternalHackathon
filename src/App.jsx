29b9abf341b45719dd2ccbca5bf69a4ddc20530f
abhineet <abhii.neat@gmail.com>
2024-01-10 14:28:48 +0530

    wow

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Feeling from "./pages/Feeling";
 
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route exact path="/" element={<HomePage />} />
-                
+                <Route exact path="/result" element={<Feeling/>} />
             </Routes>
         </Router>
     );
