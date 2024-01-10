7189598d9bb66d719a07197d40330b90fcc327b5
abhineet <abhii.neat@gmail.com>
2024-01-10 14:01:21 +0530

    added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default function HomePage() {
     return (
         <div>
             <NavBar />
-            <img src="/bg(2).png" alt="" />
+          
         </div>
     );
 }
